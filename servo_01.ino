341cbbfc623a181fe2f927b96fadc497adc8dced
I-Made-Aditya <41387126+I-Made-Aditya@users.noreply.github.com>
2018-07-19 00:06:44 +0700

    Servo
    Kodingan untuk servo

---
+++ servo_01.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo servo1; 
+int trig=2;
+int echo=3;
+
+void setup()
+{
+  servo1.attach(2); //pin sinyal servo terkoneksi pada pin 2 arduino
+}
+void loop()
+{
+  servo1.write(60);
+  delay(800);
+  servo1.write(0);
+  delay(800);
+  servo1.write(120);
+  delay(800);
+  servo1.write(60);
+  delay(800);
+}
